cd17c61f463904d1c93687b97440b7d453eb5c99
Daniel J Feleppa <dfeleppa@gmail.com>
2020-05-05 21:09:05 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   belongs_to :network
   
   def actors_list
-      actorss.collect do |actor|
+      actors.collect do |actor|
         "#{actor.first_name} - #{actor.lastname}"
       end.join(' ')
   end
