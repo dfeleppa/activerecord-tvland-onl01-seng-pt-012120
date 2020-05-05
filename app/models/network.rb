534738e75345a74db641dab413a1fa6525d412de
Daniel J Feleppa <dfeleppa@gmail.com>
2020-05-05 21:05:28 +0000

    Automatically backed up by Learn

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,7 +1,8 @@
 class Network < ActiveRecord::Base
   has_many :shows
+  has_many :actors, through: :shows
 
   def sorry
-    "We're sorry about passing on John Mulaney's pilot"
+    "We're sorry about passing on John Mullaney's pilot"
   end
 end
